a975b6d3830eabd31e54aa36f46f10bba8950797
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-04-12 22:49:14 +1000

    refreshed templated from VP core

--- docker-build.sh
+++ docker-build.sh
@@ -5,9 +5,9 @@ echo "prepare a deployment folder"
 mkdir $VP_HOME/deploy
 cp -r $VP_HOME/vocprez/* $VP_HOME/deploy
 
-echo "copy DAWE style and templates to VocPrez core"
-cp style/* $VP_HOME/vocprez/view/style
-cp templates/* $VP_HOME/vocprez/view/templates
+echo "copy DAWE style and templates to VocPrez deploy folder"
+cp style/* $VP_HOME/deploy/view/style
+cp templates/* $VP_HOME/deploy/view/templates
 
 echo "set the VocPrez config"
 sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
@@ -20,6 +20,6 @@ echo "run Dockerfile there"
 docker build -t vocprez-dawe -f $VP_HOME/Dockerfile $VP_HOME
 
 echo "clean-up"
-# rm -r $VP_HOME/deploy
+rm -r $VP_HOME/deploy
 
 echo "complete"
