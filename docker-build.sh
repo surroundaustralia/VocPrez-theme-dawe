e1ba3848326702404a8f8d096ab04335dcf9fcc7
Nicholas Car <nicholas.car@surroundaustralia.com>
2021-04-12 19:11:58 +1000

    docker-build working

---
+++ docker-build.sh
@@ -0,0 +1,24 @@
+# Mac
+PATH="/usr/local/opt/gnu-sed/libexec/gnubin:$PATH"
+
+echo "prepare a deployment folder"
+mkdir $VP_HOME/deploy
+cp -r $VP_HOME/vocprez/* $VP_HOME/deploy
+
+echo "copy DAWE style and templates to VocPrez core"
+cp style/* $VP_HOME/vocprez/view/style
+cp templates/* $VP_HOME/vocprez/view/templates
+
+echo "set the VocPrez config"
+sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
+sed -i 's#$SPARQL_USERNAME#'"$SPARQL_USERNAME"'#' $VP_THEME_HOME/config_updated.py
+sed -i 's#$SPARQL_PASSWORD#'"$SPARQL_PASSWORD"'#' $VP_THEME_HOME/config_updated.py
+mv $VP_THEME_HOME/config_updated.py $VP_HOME/deploy/_config/__init__.py
+
+echo "run Dockerfile there"
+docker build -t vocprez-dawe -f $VP_HOME/Dockerfile $VP_HOME
+
+echo "clean-up"
+rm -r $VP_HOME/deploy
+
+echo "complete"
